865a52cdb37a3d25f049237e59020dd2d4cab569
David Crenin <david.crenin@gmail.com>
2020-01-09 20:17:52 +0100

    navbar footer products

--- main/models.py
+++ main/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 
 
@@ -8,9 +7,11 @@ class Product(models.Model):
     description = models.TextField()
     price = models.FloatField()
     color = models.CharField(max_length=100)
+    size = models.CharField(max_length=15)
     visible_home = models.BooleanField(default=True)
     visible = models.BooleanField(default=True)
     created = models.DateField(auto_now_add=True)
+    slug = models.CharField(max_length=200, unique=True)
     image1 = models.FileField(upload_to='images/', null=True, blank=True)
     image2 = models.FileField(upload_to='images/', null=True, blank=True)
     image3 = models.FileField(upload_to='images/', null=True, blank=True)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Product
 
 
 class HomePage(TemplateView):
     template_name = 'home.html'
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "products.html"
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.order_by('created')
+
+
+class ProductDetailView(DetailView):
+    template_name = 'product-detail.html'
+
+    def get_object(self):
+        slug_ = self.kwargs.get("slug")
+        return get_object_or_404(Product, slug=slug_)
