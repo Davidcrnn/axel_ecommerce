f356ed5f97daed91413dc5f383a6ecbd0a515d33
David Crenin <david.crenin@gmail.com>
2020-01-13 17:56:03 +0100

    whitenoise

--- axel_ecommerce/settings.py
+++ axel_ecommerce/settings.py
@@ -158,6 +158,6 @@ STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
